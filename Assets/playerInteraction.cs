bca6dda4ad55882c9abe7eaad33aae04c4744696
SoSciupato <panuccio.niccolo@gmail.com>
2025-03-19 12:17:19 +0100

    baobab

---
+++ Assets/playerInteraction.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+
+public class playerInteraction : MonoBehaviour
+{
+
+    public GameObject weapon;
+    private weaponBehaviour wb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wb = weapon.GetComponent<weaponBehaviour>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetMouseButton(0))
+            wb.Shooting();
+
+    }
+}
--- Assets/weaponBehaviour.cs
+++ Assets/weaponBehaviour.cs
@@ -1,36 +1,93 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class weaponBehaviour : MonoBehaviour
 {
     [Header("This Weapon Stats")]
-
     public string weaponName;
     public GameObject weapon3DModel;
+    public int maxAmmo;
+    private int ammo;
+    public float reloadTime;
+    public float fireRate;
+    public float fireDistance;
 
-    public float firingSpeed;
-    public float reloadSpeed;
-
-    [Header("Extra Stats")]
-
-    private bool isMeleeWeapon;         //if this is a melee weapon
-    public bool isPlayerShooting;       //if the player is shooting while it's holding this weapon
-
-
+    [Header("Conditions")]
+    bool hasAmmo = true;
 
 
+    [Header("Important")]
+    public GameObject weaponTip;
+    public GameObject playerOrientation;
+    LayerMask hittableLayer;
 
-    // Start is called before the first frame update
-    void Start()
+    IEnumerator WeaponReload()
     {
-        isPlayerShooting = false;
-        isMeleeWeapon = false;
+        Debug.Log("Reloading..");
+        yield return new WaitForSeconds(reloadTime);
+        
+        
+        ammo = maxAmmo;
+        hasAmmo = true;
+        Debug.Log("Reloaded Ammos");
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator ShootingTimeForEachBullet()
     {
         
+        if (ammo == 0)
+        {
+            Debug.Log("Out of Ammo");
+            hasAmmo = false;
+            StartCoroutine(WeaponReload());
+
+        }
+        else
+        {
+            FixedUpdate();
+
+            yield return new WaitForSeconds(fireRate);
+            
+            ammo = ammo - 1;
+        }
+
+    }
+    
+
+
+    private void Start()
+    {
+        ammo = maxAmmo;
+        hittableLayer = LayerMask.GetMask("watIsGround","CharacterMesh");
+    }
+
+    
+    
+    public void Shooting()
+    {
+        if (hasAmmo)
+        {
+            StartCoroutine(ShootingTimeForEachBullet());
+            
+            Debug.Log(ammo);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(weaponTip.transform.position, playerOrientation.transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+        {
+            Debug.DrawRay(weaponTip.transform.position, playerOrientation.transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            
+            
+        }
+        else
+        {
+            Debug.DrawRay(weaponTip.transform.position, playerOrientation.transform.TransformDirection(Vector3.forward) * fireDistance, Color.red);
+            
+        }
     }
 }
