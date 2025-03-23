c60f87d11cc481e872fe9b07fde35d0466b6efb9
SoSciupato <panuccio.niccolo@gmail.com>
2025-03-23 01:27:59 +0100

    tweak1

--- Assets/zombie_class.cs
+++ Assets/zombie_class.cs
@@ -6,15 +6,22 @@ public class zombie_class : MonoBehaviour
 {
     [Header("Zombie Stats")]
     private float health = 100;
+    public AudioClip zDeath;
+
     public void takeDamage(float incomingDamage)
     {
         health = health - incomingDamage;
+        gameObject.GetComponent<AudioSource>().PlayOneShot(zDeath);
     }
-
+    
     private void checkToDestroySelf()
     {
         if(health <= 0)
+        {
+            
             Destroy(gameObject);
+        }
+            
 
     }
 
